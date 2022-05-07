5843b1341e2ace8b68bec196f087aae217b31050
BosaBL <christopher172003@outlook.cl>
2022-05-07 00:06:50 -0400

    Se añadiéron tipos de datos en los parámetros

--- model/Cart.py
+++ model/Cart.py
@@ -1,16 +1,17 @@
-'''
+"""
 Created on 03-05-2022
 
 @author: alvar
-'''
+"""
 
-class Cart():
-    def __init__(self,identifier,path):
-        self.__id=identifier
-        self.__imagePath=path
-        
+
+class Cart:
+    def __init__(self, identifier: int, path: str):
+        self.__id = identifier
+        self.__imagePath = path
+
     def getId(self):
         return self.__id
-    
+
     def getPath(self):
         return self.__imagePath
--- model/Menu.py
+++ model/Menu.py
@@ -1,15 +1,16 @@
-'''
+"""
 Created on 03-05-2022
 
 @author: alvar
-'''
+"""
 
-class Menu():
+
+class Menu:
     def __init__(self):
-        self.__difficulty=0
-        
-        
-    def setDifficulty(self,n):
-        self.__difficulty=n
+        self.__difficulty = 0
+
+    def setDifficulty(self, n: str):
+        self.__difficulty = n
+
     def getDifficulty(self):
         return self.__difficulty
