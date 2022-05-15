c2d6f9be34ebb23bc33b6f8ecdf778d0471df78d
BosaBL <christopher172003@outlook.cl>
2022-05-15 00:25:41 -0400

    [A-03/TP-001] Se cambió a una versión vectorial del tablero.

--- model/Board.py
+++ model/Board.py
@@ -6,7 +6,7 @@ class Board:
     def __init__(self, rows: int, cols: int):
         self.__cols = cols
         self.__rows = rows
-        self.__boardVector = [None for _ in range(cols * rows)]
+        self.__boardVector = []
 
     def getBoardMatrix(self):
         boardMatrix = [[None for _ in range(self.__cols)] for _ in range(self.__rows)]
