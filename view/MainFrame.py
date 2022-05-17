555d96793ce074efd2348e748e12ffdc7d4251e8
Alvaro Ibáñez <alvarobenjamin.ibanez@alumnos.ulagos.cl>
2022-05-17 11:46:50 -0400

    [A-02/TP-003] Se implementa funcionalidad del menu

--- view/MainFrame.py
+++ view/MainFrame.py
@@ -13,12 +13,23 @@ class MainFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, title="FindThem", style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
 
-        self.gamePanel =MenuPanel(self)
-        self.gamePanel.Show()
+        self.menuPanel = MenuPanel(self)
+        self.menuPanel.Show()
 
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer.Add(self.gamePanel)
+        self.sizer.Add(self.menuPanel)
 
         self.SetSizerAndFit(self.sizer)
 
+        self.Show()
+        
+    def startGame(self):
+        self.menuPanel.Destroy()
+        
+        self.gamePanel = GamePanel(self,3,6)
+        self.gamePanel.Show()
+        self.sizer.Add(self.gamePanel)
+        self.SetSizerAndFit(self.sizer)
+        
+        self.Centre()
         self.Show()
