69b30e90eb88a296f8f3952cd763b62f71b4fe98
BosaBL <christopher172003@outlook.cl>
2022-05-28 02:04:13 -0400

    Se arregló import erroneo

--- view/MainFrame.py
+++ view/MainFrame.py
@@ -4,14 +4,18 @@ Created on 26-04-2022
 @author: alvaro
 """
 import wx
-from view.GamePanels import GamePanel
+from view.GamePanel import GamePanel
 from view.Menu import *
-
 
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, title="FindThem", style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+        wx.Frame.__init__(
+            self,
+            None,
+            title="FindThem",
+            style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER,
+        )
 
         self.menuPanel = MenuPanel(self)
         self.menuPanel.Show()
@@ -22,14 +26,14 @@ class MainFrame(wx.Frame):
         self.SetSizerAndFit(self.sizer)
 
         self.Show()
-        
+
     def startGame(self):
         self.menuPanel.Destroy()
-        
-        self.gamePanel = GamePanel(self,3,6)
+
+        self.gamePanel = GamePanel(self, 3, 6)
         self.gamePanel.Show()
         self.sizer.Add(self.gamePanel)
         self.SetSizerAndFit(self.sizer)
-        
+
         self.Centre()
         self.Show()
