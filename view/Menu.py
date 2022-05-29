a5e60d415d4a1bcdb2682c0c846d27a4a937a64c
BosaBL <christopher172003@outlook.cl>
2022-05-28 22:02:25 -0400

    [A-02/TP-003] Se modificó la clase DifficultyFrame cambiando su tipo a Panel

--- view/Menu.py
+++ view/Menu.py
@@ -1,76 +1,66 @@
-'''
+"""
 Created on 15-05-2022
 
 @author: alvaro
-'''
+"""
 import wx
 
 
-#class of new window to choose difficulty options
-class DifficultyFrame(wx.Frame):
-    def __init__(self,title,parent):
-        wx.Frame.__init__(self,parent,title=title,size=(500,500))
+# class of new window to choose difficulty options
+class DifficultyPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size=(500, 500))
         self.parent = parent
+        self.title = wx.StaticText(self, -1, "DIFFICULTY", pos=(210, 50))
+        self.buttonEasy = wx.Button(self, -1, "EASY", pos=(190, 140), size=(100, 30))
+        self.buttonEasy.Bind(wx.EVT_BUTTON, self.easyMode)
+        self.buttonMedium = wx.Button(
+            self, -1, "MEDIUM", pos=(190, 210), size=(100, 30)
+        )
+        self.buttonHard = wx.Button(self, -1, "HARD", pos=(190, 280), size=(100, 30))
+        self.SetBackgroundColour(wx.Colour(1000, 1000, 1000))
         self.Show(True)
-        self.title=wx.StaticText(self,-1,u"DIFFICULTY", pos=(210,50))
-        self.buttonEasy=wx.Button(self,-1,U"EASY",pos=(190,140),size=(100,30))
-        self.buttonEasy.Bind(wx.EVT_BUTTON,self.easyMode)
-        self.buttonMedium=wx.Button(self,-1,U"MEDIUM",pos=(190,210),size=(100,30))
-        self.buttonHard=wx.Button(self,-1,U"HARD",pos=(190,280),size=(100,30))
-        self.SetBackgroundColour(wx.Colour(1000,1000,1000))
         self.Centre()
-        
-    def easyMode(self,evt):
-        self.parent.startGame()
+
+    def easyMode(self, evt):
         self.Destroy()
-#this class creates the window to view the scoreboard
+        self.parent.startGame()
+
+
+# this class creates the window to view the scoreboard
 class ScoreBoard(wx.Frame):
-    def __init__(self,title,parent=None):
-        wx.Frame.__init__(self,parent,title=title,size=(500,500))
+    def __init__(self, title, parent=None):
+        wx.Frame.__init__(self, parent, title=title, size=(500, 500))
         self.Show(True)
-        self.title=wx.StaticText(self,-1,u"SCOREBOARD", pos=(210,50))
-        #provisory
-    
-    
+        self.title = wx.StaticText(self, -1, "SCOREBOARD", pos=(210, 50))
+        # provisory
 
-#main window creation class to function as menu
+
+# main window creation class to function as menu
 class MenuPanel(wx.Panel):
-    def __init__(self,parent):
+    def __init__(self, parent):
         self.parent = parent
-        wx.Panel.__init__(self,parent,size=(500,500))
-        self.buttonStart = wx.Button(self,label="START",pos=(190,140),size=(100,30))
+        wx.Panel.__init__(self, parent, size=(500, 500))
+        self.buttonStart = wx.Button(
+            self, label="START", pos=(190, 140), size=(100, 30)
+        )
         self.buttonStart.Bind(wx.EVT_BUTTON, self.difficulty)
-        self.panel=wx.Panel(self,-1)
-        self.title=wx.StaticText(self,-1,u"FIND THEM", pos=(210,50))
-        self.buttonScoreBoard=wx.Button(self,-1,U"SCOREBOARD",pos=(190,210),size=(100,30))
+        self.panel = wx.Panel(self, -1)
+        self.title = wx.StaticText(self, -1, "FIND THEM", pos=(210, 50))
+        self.buttonScoreBoard = wx.Button(
+            self, -1, "SCOREBOARD", pos=(190, 210), size=(100, 30)
+        )
         self.buttonScoreBoard.Bind(wx.EVT_BUTTON, self.scoreboard)
-        self.buttonExit=wx.Button(self,-1,U"EXIT",pos=(190,280),size=(100,30))
+        self.buttonExit = wx.Button(self, -1, "EXIT", pos=(190, 280), size=(100, 30))
         self.buttonExit.Bind(wx.EVT_BUTTON, self.delete)
-        
-        
-        
-    
-    def difficulty(self,event):
-        self.title = "FIND THEM"
-        self.frame = DifficultyFrame(title=self.title, parent=self.parent)
-        
-    def delete(self,event):
+
+    def difficulty(self, event):
+        self.Destroy()
+        self.panel = DifficultyPanel(self.parent)
+
+    def delete(self, event):
         self.parent.Destroy()
-        
-    def scoreboard(self,event):
-        self.title="FIND THEM"
-        self.frame=ScoreBoard(title=self.title)
-    
-    
-    
-                
 
-
-
-
-        
-        
-
-
- 
-    
+    def scoreboard(self, event):
+        self.title = "FIND THEM"
+        self.frame = ScoreBoard(title=self.title)
