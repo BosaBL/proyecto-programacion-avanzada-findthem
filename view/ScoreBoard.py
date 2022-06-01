272d4f966f1dfc54edc109b705a94b35fad467e2
matias <matiasjohan.toro@alumnos.ulagos.cl>
2022-06-01 08:33:41 -0400

    [A-06/TP-004] columna de player

--- view/ScoreBoard.py
+++ view/ScoreBoard.py
@@ -6,26 +6,25 @@ Created on 30-05-2022
 import wx
 import wx.grid as wxgrid
 
-class ScoreBoardController(wx.Frame):
+class ScoreBoard(wx.Frame):
     def __init__(self,parent,title):
         wx.Frame.__init__(self,parent=parent,title=title,size=(800,800))
 
         row=100
-        column=1
+        column=2
         self.grid=wxgrid.Grid(self, -1)
         self.grid.CreateGrid(row,column)
         self.grid.EnableEditing(False)
-        header=["Score"]
-        width=[100, 100]
         
+        header=["Player", "Score"]
+        width=[100, 100]
         for n,col in enumerate(range(column)):
             self.grid.SetColLabelValue(col,header[n])
             self.grid.SetColSize(col,width[n])
         
         self.Centre(True)
         self.Show()
-        
-        
+             
 app=wx.App()
-frame=ScoreBoardController(None,"ScoreBoard")
+frame=ScoreBoard(None,"ScoreBoard")
 app.MainLoop()
