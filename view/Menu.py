0e6d4a68d88afbf7cfbc2681d381d64b71acc6cf
Alvaro Ibáñez <alvarobenjamin.ibanez@alumnos.ulagos.cl>
2022-06-20 03:22:50 -0400

    [A-02/TP-003] Se modifica la estetica de la interfaz y los accesos al botón de dificultad y tablero.

--- view/Menu.py
+++ view/Menu.py
@@ -10,41 +10,64 @@ import wx
 class DifficultyPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, size=(500, 500))
+        self.SetBackgroundColour("turquoise")
         self.parent = parent
-        self.title = wx.StaticText(self, -1, "DIFFICULTY", pos=(210, 50))
-        self.buttonEasy = wx.Button(self, -1, "EASY", pos=(190, 140), size=(100, 30))
+        self.title = wx.StaticText(self, -1, "DIFICULTAD", pos=(180, 50))
+        self.count=18
+        self.font=wx.Font(self.count,wx.FONTFAMILY_DEFAULT,
+                          wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL)
+        self.title.SetFont(self.font)
+        self.buttonEasy = wx.Button(self, -1, "FACIL", pos=(190, 140), size=(100, 30))
         self.buttonEasy.Bind(wx.EVT_BUTTON, self.easyMode)
         self.buttonMedium = wx.Button(
-            self, -1, "MEDIUM", pos=(190, 210), size=(100, 30)
+            self, -1, "MEDIO", pos=(190, 210), size=(100, 30)
         )
-        self.buttonMedium.Bind(wx.EVT_BUTTON, self.mediumMode)
-        self.buttonHard = wx.Button(self, -1, "HARD", pos=(190, 280), size=(100, 30))
-        self.buttonHard.Bind(wx.EVT_BUTTON, self.hardMode)
-        self.SetBackgroundColour(wx.Colour(1000, 1000, 1000))
+        
+        self.buttonHard = wx.Button(self, -1, "DIFICIL", pos=(190, 280), size=(100, 30))
+        
+        self.buttonBack = wx.Button(
+            self, label="VOLVER", pos=(190, 400), size=(100, 30)
+        )
+        self.buttonBack.Bind(wx.EVT_BUTTON, self.back)
+        
         self.Show(True)
         self.Centre()
+        
+        
+    def back(self,evt):
+        self.Destroy()
+        self.parent.showMenu()
 
     def easyMode(self, evt):
         self.Destroy()
         self.parent.startEasyGame()
     
-    def mediumMode(self, evt):
-        self.Destroy()
-        self.parent.startMediumGame()
-        
-    def hardMode(self, evt):
-        self.Destroy()
-        self.parent.startHardGame()
-
-        
     
+        
     
 # this class creates the window to view the scoreboard
-class ScoreBoard(wx.Frame):
-    def __init__(self, title, parent=None):
-        wx.Frame.__init__(self, parent, title=title, size=(500, 500))
-        self.Show(True)
-        self.title = wx.StaticText(self, -1, "SCOREBOARD", pos=(210, 50))
+class ScoreBoard(wx.Panel):
+    def __init__(self, parent):
+        self.parent=parent
+        wx.Panel.__init__(self, parent, size=(500, 500))
+        self.SetBackgroundColour("turquoise")
+        self.title = wx.StaticText(self, -1, "TABLA DE PUNTAJE", pos=(130, 50))
+        self.count=18
+        self.font=wx.Font(self.count,wx.FONTFAMILY_DEFAULT,
+                          wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL)
+        self.title.SetFont(self.font)
+        
+        
+        self.buttonBack = wx.Button(
+            self, label="VOLVER", pos=(190, 450), size=(100, 30)
+        )
+        self.buttonBack.Bind(wx.EVT_BUTTON, self.back)
+        
+    def back(self,evt):
+        self.Destroy()
+        self.parent.showMenu()
+            
+    
         # provisory
 
 
@@ -53,17 +76,23 @@ class MenuPanel(wx.Panel):
     def __init__(self, parent):
         self.parent = parent
         wx.Panel.__init__(self, parent, size=(500, 500))
+        self.SetBackgroundColour("turquoise")
+        self.title = wx.StaticText(self, -1, "FIND THEM", pos=(180, 50))
+        self.count=18
+        self.font=wx.Font(self.count,wx.FONTFAMILY_DEFAULT,
+                          wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL)
+        self.title.SetFont(self.font)
+        
         self.buttonStart = wx.Button(
-            self, label="START", pos=(190, 140), size=(100, 30)
+            self, label="JUGAR", pos=(190, 140), size=(100, 30)
         )
         self.buttonStart.Bind(wx.EVT_BUTTON, self.difficulty)
         self.panel = wx.Panel(self, -1)
-        self.title = wx.StaticText(self, -1, "FIND THEM", pos=(210, 50))
         self.buttonScoreBoard = wx.Button(
-            self, -1, "SCOREBOARD", pos=(190, 210), size=(100, 30)
+            self, -1, "PUNTAJES", pos=(190, 210), size=(100, 30)
         )
         self.buttonScoreBoard.Bind(wx.EVT_BUTTON, self.scoreboard)
-        self.buttonExit = wx.Button(self, -1, "EXIT", pos=(190, 280), size=(100, 30))
+        self.buttonExit = wx.Button(self, -1, "SALIR", pos=(190, 280), size=(100, 30))
         self.buttonExit.Bind(wx.EVT_BUTTON, self.delete)
 
     def difficulty(self, event):
@@ -73,6 +102,9 @@ class MenuPanel(wx.Panel):
     def delete(self, event):
         self.parent.Destroy()
 
-    def scoreboard(self, event):
-        self.title = "FIND THEM"
-        self.frame = ScoreBoard(title=self.title)
+    
+    def scoreboard(self, evt):
+        self.Destroy()
+        self.parent.openScoreboard()
+        
+        
