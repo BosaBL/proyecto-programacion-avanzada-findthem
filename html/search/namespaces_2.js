f3edfb64f5ae46a4d182352a82797f42e08dd942
BosaBL <christopher172003@outlook.cl>
2022-06-22 00:24:13 -0400

    Se actualiza documentación

--- html/search/namespaces_2.js
+++ html/search/namespaces_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['gamepanel_0',['GamePanel',['../namespaceview_1_1_game_panel.html',1,'view']]],
   ['mainframe_1',['MainFrame',['../namespaceview_1_1_main_frame.html',1,'view']]],
   ['menu_2',['Menu',['../namespaceview_1_1_menu.html',1,'view']]],
-  ['view_3',['view',['../namespaceview.html',1,'']]]
+  ['scoreboard_3',['ScoreBoard',['../namespaceview_1_1_score_board.html',1,'view']]],
+  ['view_4',['view',['../namespaceview.html',1,'']]]
 ];
